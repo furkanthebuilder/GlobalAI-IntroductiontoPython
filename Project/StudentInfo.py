430d6f4a0919c78a3149316a0f31f821ab692a9b
Furkan Edizkan <65301915+furkanthebuilder@users.noreply.github.com>
2020-12-26 20:00:21 +0300

    Homework Project

---
+++ Project/StudentInfo.py
@@ -0,0 +1,18 @@
+StudentName = [
+    'USER'
+]
+
+StudentSurname = [
+    'GUEST'
+]
+
+Courses = []
+
+
+Grades = [
+    {'Course':'DERS1', 'midterm':60, 'final':70, 'project':80},
+    {'Course':'DERS2', 'midterm':100,'final':95,'project':100},
+    {'Course':'DERS3', 'midterm':45, 'final':30, 'project':10},
+    {'Course':'DERS4', 'midterm':10,'final':75,'project':25},
+    {'Course':'DERS5', 'midterm':80, 'final':45, 'project':50}
+]
